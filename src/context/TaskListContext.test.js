b371b8cae6fae7164b91e51d72af88a242940fbf
neqrapi <60574161+neqrapi@users.noreply.github.com>
2020-04-22 22:55:19 +0200

    deploy my react todolist

---
+++ src/context/TaskListContext.test.js
@@ -0,0 +1,7 @@
+import React from "react"
+import {render} from "@testing-library/react"
+import TaskListContext from "../context/TaskListContext"
+describe('addTask',()=>{
+expect(task[task.lenght-1].toBe('Pierwszy test'))
+
+})
